65e47b6cbec634b580a42936bb8ce01e04eda650
Tiozao <marco_aurelioo@yahoo.com.br>
2021-12-03 21:12:13 -0300

    Redução complexidade classes

--- AppWithSwagger/src/main/java/com/tiozao/teste/AppWithSwagger/application/controller/ProfileController.java
+++ AppWithSwagger/src/main/java/com/tiozao/teste/AppWithSwagger/application/controller/ProfileController.java
@@ -1,59 +1,42 @@
 package com.tiozao.teste.AppWithSwagger.application.controller;
 
 import com.tiozao.teste.AppWithSwagger.application.controller.model.ProfileDto;
+import com.tiozao.teste.AppWithSwagger.application.domain.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.management.InvalidAttributeValueException;
 import javax.websocket.server.PathParam;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 @RestController
 public class ProfileController {
 
-    Map<String,ProfileDto> map_profile = new HashMap<String,ProfileDto>();
+    @Autowired
+    private ProfileService service;
 
     @GetMapping("/{id_profile}")
     public ResponseEntity<ProfileDto> findProfile(@PathVariable("id_profile") String id_profile){
-        ProfileDto profile = map_profile.get(id_profile);
-        if(profile!= null){
-            return ResponseEntity.ok(profile);
-        }
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(service.findProfile(id_profile));
     }
 
     @PostMapping
     public ResponseEntity<ProfileDto> createProfile(
             @RequestBody ProfileDto profileDto
             ){
-        profileDto.setId(UUID.randomUUID().toString());
-        map_profile.put(profileDto.getId(),profileDto);
-        return ResponseEntity.ok(profileDto);
+        return ResponseEntity.ok(service.createProfile(profileDto));
     }
 
     @PutMapping("/{id_profile}")
     public ResponseEntity<ProfileDto> updateProfile(
             @PathParam("id_profile") String id_profile,
-            @RequestBody ProfileDto profile_n ){
-        ProfileDto profile_s = map_profile.get(id_profile);
-        if(profile_s == null){
-            return ResponseEntity.notFound().build();
-        }
-        if(profile_s.getId() != profile_n.getId()){
-            return ResponseEntity.unprocessableEntity().build();
-        }
-        map_profile.put(id_profile,profile_n);
-        return ResponseEntity.ok(profile_n);
+            @RequestBody ProfileDto profile_n ) throws InvalidAttributeValueException {
+        return ResponseEntity.ok(service.updateProfile(id_profile,profile_n));
     }
 
     @DeleteMapping("/{id_profile}")
     public ResponseEntity<ProfileDto> delete(@PathParam("id_profile") String id_profile){
-        if(map_profile.get(id_profile) != null){
-            map_profile.remove(id_profile);
-        }else{
-            return ResponseEntity.notFound().build();
-        }
+        service.deleteProfile(id_profile);
         return ResponseEntity.ok().build();
     }
 
---
+++ AppWithSwagger/src/main/java/com/tiozao/teste/AppWithSwagger/application/domain/ProfileService.java
@@ -0,0 +1,50 @@
+package com.tiozao.teste.AppWithSwagger.application.domain;
+
+import com.tiozao.teste.AppWithSwagger.application.controller.model.ProfileDto;
+import org.springframework.stereotype.Service;
+import org.webjars.NotFoundException;
+
+import javax.management.InvalidAttributeValueException;
+import java.io.InvalidObjectException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+public class ProfileService {
+
+    Map<String,ProfileDto> map_profile = new HashMap<String,ProfileDto>();
+
+    public ProfileDto findProfile(String id_profile){
+       return getProfile(id_profile);
+    }
+
+    public ProfileDto createProfile( ProfileDto profileDto){
+        profileDto.setId(UUID.randomUUID().toString());
+        map_profile.put(profileDto.getId(),profileDto);
+        return profileDto;
+    }
+
+    public ProfileDto updateProfile(String id_profile, ProfileDto profileDto)
+            throws InvalidAttributeValueException {
+        ProfileDto profile_old = getProfile(id_profile);
+        if(profile_old.getId() != id_profile){
+            throw new InvalidAttributeValueException("Parametros invalidos");
+        }
+        map_profile.put(id_profile,profileDto);
+        return profileDto;
+    }
+
+    private ProfileDto getProfile(String id_profile) {
+        ProfileDto profile = map_profile.get(id_profile);
+        if(profile == null){
+            throw new NotFoundException("profile não encontrado! id '"+id_profile+"'");
+        }
+        return profile;
+    }
+
+    public void deleteProfile(String id_profile) {
+        getProfile(id_profile);
+        map_profile.remove(id_profile);
+    }
+}
